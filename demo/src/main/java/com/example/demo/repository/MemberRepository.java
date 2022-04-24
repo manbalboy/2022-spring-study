72f097b0d5eee9db96cc1b63b12ae9fd91c0af21
manbalboy <manbalboy@hanmail.net>
2022-04-24 12:29:43 +0900

    스프링 입문 스프링 빈과 의존관계

--- demo/src/main/java/com/example/demo/repository/MemberRepository.java
+++ demo/src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface MemberRepository {
     Member save(Member member);
