f256aef70577454a77c335bd620dd9e53e243304
manbalboy <manbalboy@hanmail.net>
2022-05-01 10:19:32 +0900

    jdbc template 적용

--- demo/src/main/java/com/example/demo/SpringConfig.java
+++ demo/src/main/java/com/example/demo/SpringConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.repository.JdbcMemberRepository;
+import com.example.demo.repository.JdbcTemplateMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +23,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
