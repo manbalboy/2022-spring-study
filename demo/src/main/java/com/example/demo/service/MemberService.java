40e0a1cbd88bc6a225d65f5ef1c17774e2c77612
manbalboy <manbalboy@hanmail.net>
2022-05-02 21:28:14 +0900

    스프링 입문  jpa

--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
