c9805441630888bb247cc364819803aad2f7a6b0
manbalboy <manbalboy@hanmail.net>
2022-05-10 21:35:25 +0900

    AppConfig 리팩터링

--- springStudy/src/main/java/hello/core/config/AppConfig.java
+++ springStudy/src/main/java/hello/core/config/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core.config;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.Order;
@@ -9,10 +10,18 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public OrderService orderService () {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 }
