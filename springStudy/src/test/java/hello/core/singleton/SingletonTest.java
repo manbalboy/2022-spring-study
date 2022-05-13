2b402479cc1a72354505a9a3c3004fc818475cba
manbalboy <manbalboy@hanmail.net>
2022-05-13 22:33:02 +0900

    singleton 학습2

--- springStudy/src/test/java/hello/core/singleton/SingletonTest.java
+++ springStudy/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +36,16 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService).isSameAs(memberService1);
     }
 }
