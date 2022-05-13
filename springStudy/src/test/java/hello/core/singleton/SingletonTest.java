ab9f83604941d967302156ffef71ae1ab6dec746
manbalboy <manbalboy@hanmail.net>
2022-05-13 22:02:59 +0900

    singleton 학습

---
+++ springStudy/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import hello.core.config.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+
+        MemberService memberService = appConfig.memberService();
+        MemberService memberService1 = appConfig.memberService();
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService1 = " + memberService1);
+
+        assertThat(memberService).isNotSameAs(memberService1);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+}
